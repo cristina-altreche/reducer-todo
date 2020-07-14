e02b5c3edf90844a3000697b0a65eaba575455c6
Cristina Altreche <c.altreche@gmail.com>
2020-07-13 21:56:01 -0400

    styling almost complete

--- todo/src/components/TodoForm.js
+++ todo/src/components/TodoForm.js
@@ -1,35 +1,40 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-export const TodoForm = ({dispatch}) => {
-  const [items, setItems] = useState('');
+export const TodoForm = ({ dispatch }) => {
+  const [items, setItems] = useState("");
 
-  const handleChanges = e => {
+  const handleChanges = (e) => {
     setItems(e.target.value);
   };
 
-  const submitForm = e => {
+  const submitForm = (e) => {
     e.preventDefault();
     dispatch({
-      type: 'AddTodo',
-      payload: [items]
+      type: "AddTodo",
+      payload: [items],
     });
   };
 
-  const clearCompleted = e => {
+  const clearCompleted = (e) => {
     e.preventDefault();
     dispatch({
-      type: 'ClearCompleted'
+      type: "ClearCompleted",
     });
   };
 
   return (
-    <div>
+    <div className="form-container">
+      <div className="banner">Today Todo</div>
       <form onSubmit={submitForm}>
-        <label>Todo</label>
-        <input name='todo' onChange={handleChanges} value={items} id={Date.now()} />
+        <input
+          name="todo"
+          onChange={handleChanges}
+          value={items}
+          id={Date.now()}
+        />
         <button>Add Item</button>
         <button onClick={clearCompleted}>Clear Item</button>
       </form>
     </div>
   );
-}
+};
